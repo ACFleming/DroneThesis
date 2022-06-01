f34f11d219629e1b24e780ec51aaff58525070fd
ACFleming <flemingoflemingoflemingo@gmail.com>
2022-06-01 14:34:24 +1000

    Creating Agent class

---
+++ Agent.cpp
@@ -0,0 +1,6 @@
+#include "Agent.hpp"
+
+
+Coord Agent::attemptMove() {
+    return Coord(4,4, "Dummy");
+}
--- Agent.hpp
+++ Agent.hpp
@@ -18,6 +18,7 @@ private:
 public:
     Agent(/* args */);
     ~Agent();
+    Coord attemptMove();
 };
 
 
