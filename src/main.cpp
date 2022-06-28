36d401562b32bdebf62afddfb833c587d5bccc8a
ACFleming <flemingoflemingoflemingo@gmail.com>
2022-06-28 16:59:22 +1000

    Using approx poly to get corners of merged octagons

--- src/main.cpp
+++ src/main.cpp
@@ -20,38 +20,102 @@ int main (int argc, char* argv[]){
     Field f = Field(field_x_width,field_y_length,num_sources, std_dev_noise, max_range);  
     Agent a1 = Agent("Drone1", 0, 0, field_x_width, field_y_length, max_range,speed, bearing);
 
-    std::vector<cv::Point2i> ret = a1.getOctagonPoints(200,100);
-
-    for(auto &p: ret){
-        std::cout << p.x << "," << p.y << std::endl;
-    }
-
-    std::cout << "Area:" <<  cv::contourArea(ret) << std::endl;
 
 
     //rows = 600, cols = 900
-    cv::Mat img = cv::Mat::zeros(600,900,CV_8UC1);
+    cv::Mat img = cv::Mat::zeros(600,900,CV_8UC3);
 
-    
+    std::vector<cv::Point2i> oct1 = a1.getOctagonPoints(0,100);
+
+    for(auto &p: oct1){
+        std::cout << p.x << "," << p.y << std::endl;
+    }
+
+    std::cout << "Area:" <<  cv::contourArea(oct1) << std::endl;
+
+
     std::vector<std::vector<cv::Point2i>> ctrs;
 
-    // std::vector<cv::Point2i> points;
-    // points.push_back(cv::Point2i(10,10));
-    // points.push_back(cv::Point2i(100,10));
-    // points.push_back(cv::Point2i(100,100));
-    // points.push_back(cv::Point2i(10,100));
+
+    ctrs.push_back(oct1);
 
 
-    ctrs.push_back(ret);
 
-    cv::Scalar color( 255, 0,255);
-
-    cv::drawContours(img, ctrs, -1,color );
+    cv::drawContours(img, ctrs, -1,cv::Scalar(255,0,255) );
 
 
 
     cv::imshow("IMAGE", img);
     cv::waitKey(0);
+
+
+    std::vector<cv::Point2i> oct2 = a1.getOctagonPoints(50,100);
+
+    for(auto &p: oct2){
+        std::cout << p.x << "," << p.y << std::endl;
+    }
+
+    std::cout << "Area:" <<  cv::contourArea(oct2) << std::endl;
+
+
+    std::vector<std::vector<cv::Point2i>> ctrs2;
+
+
+
+
+    ctrs2.push_back(oct2);
+
+
+    cv::drawContours(img, ctrs2, -1,cv::Scalar(0,0,255) );
+
+
+
+    cv::imshow("IMAGE", img);
+    cv::waitKey(0);
+
+    std::vector<std::vector<cv::Point2i>> found_contours;
+
+    cv::Mat gray = cv::Mat::zeros(600,900,CV_8UC1);;
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    
+
+    cv::findContours(gray,found_contours,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_SIMPLE);
+
+    std::cout << "Found contours count: ";
+    int count = 0;
+    for(auto &v: found_contours){
+        count += v.size();
+    }
+    std::cout << count << std::endl;
+    
+    std::vector<std::vector<cv::Point2i>> approx;
+    
+    for(auto &c: found_contours){
+        std::vector<cv::Point2i> a;
+        cv::approxPolyDP(c, a, 0.005 * cv::arcLength(c, true), true);
+        approx.push_back(a);
+    }
+
+    std::cout << "Approx contours count: ";
+    count = 0;
+    for(auto &v: approx){
+        count += v.size();
+
+    }
+    std::cout << count << std::endl;
+    
+    cv::Mat img2 = cv::Mat::zeros(600,900,CV_8UC1);;
+    cv::drawContours(img2, approx, -1,cv::Scalar(255,0,0) );
+    
+    std::cout << "Area:" <<  cv::contourArea(approx[0]) << std::endl;
+   
+
+    cv::imshow("IMAGE2", img2);
+    cv::waitKey(0);
+
+
+
+
 
     // Agent a2 = Agent("Drone2", 0.75*field_x_width, 0.75*field_y_length, field_x_width, field_y_length);
     
